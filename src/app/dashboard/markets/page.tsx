2d4054fcdf78c0584870c4c8812aacc92a2d563b
Tanvi <tanviagrawal9913@gmail.com>
2026-08-10 11:39:38 +0530

    made fixes in dashboard pages

--- src/app/dashboard/markets/page.tsx
+++ src/app/dashboard/markets/page.tsx
@@ -1,4 +1,6 @@
-import DashboardMarkets from "../../../components/dashboard/pages/DashboardMarkets";
+// import DashboardMarkets from "../../../components/dashboard/pages/DashboardMarkets";
+
+import DashboardMarkets from "@/components/dashboard/pages/DashboardMarkets";
 
 export default function DashboardMarketsPage() {
   return <DashboardMarkets />;
